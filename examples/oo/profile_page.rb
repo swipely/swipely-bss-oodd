e9e1e19a5783a2426b22440b0460d7f632c5494c
danhodge <dan@swipely.com>
2012-12-04 09:19:26 -0500

    filled in example

--- examples/oo/profile_page.rb
+++ examples/oo/profile_page.rb
@@ -1,4 +1,9 @@
 class ProfilePage
+  def initialize(subject)
+    @subject_type = subject.subject_type
+    @subject = subject
+  end
+
   def description
     case @subject_type
     when 'User'
@@ -10,5 +15,30 @@ class ProfilePage
     else
       raise "Unrecognized type #{subject_type}"
     end
+  end
+end
+
+class User
+  attr_accessor :user_bio
+  
+  def subject_type
+    self.class.name
+  end
+end
+
+
+class Merchant
+  attr_accessor :description
+
+  def subject_type
+    self.class.name
+  end
+end
+
+class MetroArea
+  attr_accessor :city, :state
+
+  def subject_type
+    'SuperLocale'
   end
 end
